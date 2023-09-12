3dccd8a5bc1eb78550829bc9e314c7fe2b40d18c
vhperaltaj <victor.peralta@microside.com>
2023-09-12 14:19:11 -0600

    Fixed encoding, updated comments

--- Proyecto/PIC18F45K50/14_MOTOR_INFRARROJO.X/main.c
+++ Proyecto/PIC18F45K50/14_MOTOR_INFRARROJO.X/main.c
@@ -43,37 +43,15 @@
 
 #include "mcc_generated_files/mcc.h"
 
-/*
-                         Main application
- */
 void main( void )
 {
     uint16_t VALOR_ADC;
-    // Initialize the device
-    SYSTEM_Initialize();
 
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
-    // Use the following macros to:
-
-    // Enable the Global Interrupts
-    // INTERRUPT_GlobalInterruptEnable();
-
-    // Disable the Global Interrupts
-    // INTERRUPT_GlobalInterruptDisable();
-
-    // Enable the Peripheral Interrupts
-    // INTERRUPT_PeripheralInterruptEnable();
-
-    // Disable the Peripheral Interrupts
-    // INTERRUPT_PeripheralInterruptDisable();
+    SYSTEM_Initialize();                                     // Configura el dispositivo
 
     while ( 1 ) {
-        VALOR_ADC = ADC_GetConversion( channel_AN3 ) * 1.53; // Escala el valor analÃ³gico a el ciclo PWM
+        VALOR_ADC = ADC_GetConversion( channel_AN3 ) * 1.53; // Escala el valor analógico a el ciclo PWM
         if ( VALOR_ADC < 1024 )
-            EPWM1_LoadDutyValue( VALOR_ADC );                // Genera la seÃ±al PWM
+            EPWM1_LoadDutyValue( VALOR_ADC );                // Genera la señal PWM
     }
 }
-/**
- End of File
- */
